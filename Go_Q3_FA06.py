ec048d183029e158243ab2e392b52fe0e153fd74
JuliannaLGo <juliannabeatrice.go@smc.pshs.edu.ph>
2026-01-19 10:36:46 +0800

    Capitalize name and subject inputs

--- Go_Q3_FA06.py
+++ Go_Q3_FA06.py
@@ -1,10 +1,12 @@
 student = {}
 
 name = input("Enter your name: ")
+name = name.capitalize()
 student["Name"] = name
 age = input("Enter your age: ")
 student["Age"] = age
 subj = input("Enter your favorite subject: ")
+subj = subj.capitalize()
 student["Favorite Subject"] = subj
 
 print()
